7b475d511f46faf3f373eebabc7bd7f24aa487c3
Julianne Vela <hello@juliannevela.dev>
2022-05-23 17:50:42 -0700

    Initial beginning structure for Repli OT Catchup Challenge.

---
+++ Replit/index.js
@@ -0,0 +1,6 @@
+function isValid(previous, current, ops) {
+    // Compare previous string to current string using the given ops. 
+    // if the two strings are the same, return true as the action isValid. 
+
+    
+}
